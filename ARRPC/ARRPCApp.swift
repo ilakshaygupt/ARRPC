e6b7c23361e092770eb38cf0506b73d053378ea4
lakshay <gupta22154006@akgec.ac.in>
2025-01-14 14:07:49 +0530

    Initial Commit

---
+++ ARRPC/ARRPCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARRPCApp.swift
+//  ARRPC
+//
+//  Created by Lakshay Gupta on 14/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ARRPCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARRPC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ARRPC
+//
+//  Created by Lakshay Gupta on 14/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
